4041877d296593badf9c5f8dc7b40eaa31d4c8d2
James Andrews <james@jgandrews.com>
2024-12-24 18:08:44 +0000

    Updating Puzzle 6

--- Puzzle6/puzzle6.go
+++ Puzzle6/puzzle6.go
@@ -14,7 +14,7 @@ type Position struct {
 	direction string
 }
 
-type Path []Position
+type Path map[Position]struct{}
 
 func readInput(inputFile string) map[int][]rune {
 	// Read the file line by line and return the data as a slice
@@ -58,21 +58,46 @@ func findStart(guardMap map[int][]rune) Position {
 	return startPos
 }
 
-// Append a string to a slice of strings if it doesn't already exist
-func appendIfMissing(slice []string, i string) []string {
-	for _, ele := range slice {
-		if ele == i {
-			return slice
+func getPath(guardMap map[int][]rune, startPos Position) Path {
+
+	visited := Path{}
+
+	move := true
+	currentPosition := startPos
+
+	visited[currentPosition] = struct{}{}
+
+	// Move the guard
+	for move == true {
+		move, currentPosition = doMove(currentPosition, guardMap)
+
+		if move {
+			visited[currentPosition] = struct{}{}
 		}
+
 	}
-	return append(slice, i)
+
+	return visited
 }
 
-// Not sure if I need to use strings here. But it works. Makes the appendIfMissing function easier maybe?
+func countUniquePositions(path Path) int {
+
+	coordsList := make(map[string]struct{})
+
+	for pos := range path {
+
+		coords := fmt.Sprintf("%d,%d", pos.x, pos.y)
+		coordsList[coords] = struct{}{}
+	}
+
+	return len(coordsList)
+}
+
 func pos2string(pos Position) string {
 	return fmt.Sprintf("%d,%d", pos.x, pos.y)
 }
 
+// Move the guard. With hindsight this could be a lot simpler!
 func doMove(currentPosition Position, guardMap map[int][]rune) (bool, Position) {
 
 	// Move the guard
@@ -82,17 +107,15 @@ func doMove(currentPosition Position, guardMap map[int][]rune) (bool, Position) 
 	case "U":
 		// Check if the position above is blocked if not update current position to the position above
 		if currentPosition.y-1 >= 0 {
+
 			if guardMap[currentPosition.y-1][currentPosition.x] != '#' {
 				currentPosition = Position{currentPosition.x, currentPosition.y - 1, "U"}
 
 			} else {
 				// Move right
-				if currentPosition.x+1 < len(guardMap[currentPosition.y]) {
-					currentPosition = Position{currentPosition.x + 1, currentPosition.y, "R"}
-				} else {
-					// gone off the map
-					canMove = false
-				}
+
+				currentPosition = Position{currentPosition.x, currentPosition.y, "R"}
+
 			}
 
 		} else {
@@ -108,12 +131,9 @@ func doMove(currentPosition Position, guardMap map[int][]rune) (bool, Position) 
 
 			} else {
 				// Move left
-				if currentPosition.x-1 >= 0 {
-					currentPosition = Position{currentPosition.x - 1, currentPosition.y, "L"}
-				} else {
-					// gone off the map
-					canMove = false
-				}
+
+				currentPosition = Position{currentPosition.x, currentPosition.y, "L"}
+
 			}
 		} else {
 			// gone off the map
@@ -128,12 +148,9 @@ func doMove(currentPosition Position, guardMap map[int][]rune) (bool, Position) 
 
 			} else {
 				// Move up
-				if currentPosition.y-1 >= 0 {
-					currentPosition = Position{currentPosition.x, currentPosition.y - 1, "U"}
-				} else {
-					// gone off the map
-					canMove = false
-				}
+
+				currentPosition = Position{currentPosition.x, currentPosition.y, "U"}
+
 			}
 		} else {
 			// gone off the map
@@ -148,12 +165,9 @@ func doMove(currentPosition Position, guardMap map[int][]rune) (bool, Position) 
 
 			} else {
 				// Move down
-				if currentPosition.y+1 < len(guardMap) {
-					currentPosition = Position{currentPosition.x, currentPosition.y + 1, "D"}
-				} else {
-					// gone off the map
-					canMove = false
-				}
+
+				currentPosition = Position{currentPosition.x, currentPosition.y, "D"}
+
 			}
 		} else {
 			// gone off the map
@@ -162,175 +176,6 @@ func doMove(currentPosition Position, guardMap map[int][]rune) (bool, Position) 
 	}
 
 	return canMove, currentPosition
-}
-
-func part1_old(inputfile string) (int, Path) {
-	totalPositions := 0
-	guardMap := readInput(inputfile)
-
-	// This was used to solve part 1. Could replace with the path slice instead?
-	visitedPositions := make([]string, 0)
-	moveDirection := "U"
-	path := make(Path, 0)
-
-	currentPosition := Position{0, 0, "U"}
-
-	// Find the Staring position
-	for lineNo, line := range guardMap {
-		if strings.Contains(string(line), "^") {
-			guardPos := strings.Index(string(line), "^")
-			currentPosition = Position{guardPos, lineNo, "U"}
-
-			visitedPositions = appendIfMissing(visitedPositions, pos2string(currentPosition))
-			path = append(path, currentPosition)
-			break
-		}
-	}
-
-	// Move the guard
-	move := true
-	for move == true {
-
-		switch moveDirection {
-		case "U":
-			// Check if the position above is blocked if not update current position to the position above
-			if currentPosition.y-1 >= 0 {
-				if guardMap[currentPosition.y-1][currentPosition.x] != '#' {
-					currentPosition = Position{currentPosition.x, currentPosition.y - 1, "U"}
-					path = append(path, currentPosition)
-
-				} else {
-					// Move right
-					moveDirection = "R"
-				}
-
-			} else {
-				// gone off the map
-				move = false
-			}
-
-		case "D":
-			// Move down
-			if currentPosition.y+1 < len(guardMap) {
-				if guardMap[currentPosition.y+1][currentPosition.x] != '#' {
-					currentPosition = Position{currentPosition.x, currentPosition.y + 1, "D"}
-					path = append(path, currentPosition)
-
-				} else {
-					// Move left
-					moveDirection = "L"
-				}
-			} else {
-				// gone off the map
-				move = false
-			}
-
-		case "L":
-			// Move left
-			if currentPosition.x-1 >= 0 {
-				if guardMap[currentPosition.y][currentPosition.x-1] != '#' {
-					currentPosition = Position{currentPosition.x - 1, currentPosition.y, "L"}
-					path = append(path, currentPosition)
-
-				} else {
-					// Move up
-					moveDirection = "U"
-				}
-			} else {
-				// gone off the map
-				move = false
-			}
-
-		case "R":
-			// Move right
-			if currentPosition.x+1 < len(guardMap[currentPosition.y]) {
-				if guardMap[currentPosition.y][currentPosition.x+1] != '#' {
-					currentPosition = Position{currentPosition.x + 1, currentPosition.y, "R"}
-					path = append(path, currentPosition)
-
-				} else {
-					// Move down
-					moveDirection = "D"
-				}
-			} else {
-				// gone off the map
-				move = false
-			}
-		}
-
-		visitedPositions = appendIfMissing(visitedPositions, pos2string(currentPosition))
-
-	}
-
-	totalPositions = len(visitedPositions)
-	return totalPositions, path
-}
-
-func part1(inputfile string) (int, Path) {
-	totalPositions := 0
-	guardMap := readInput(inputfile)
-
-	// This was used to solve part 1. Could replace with the path slice instead?
-	visitedPositions := make([]string, 0)
-
-	path := make(Path, 0)
-
-	currentPosition := Position{0, 0, "U"}
-
-	// Find the Staring position
-	for lineNo, line := range guardMap {
-		if strings.Contains(string(line), "^") {
-			guardPos := strings.Index(string(line), "^")
-			currentPosition = Position{guardPos, lineNo, "U"}
-
-			visitedPositions = appendIfMissing(visitedPositions, pos2string(currentPosition))
-			path = append(path, currentPosition)
-			break
-		}
-	}
-
-	// Move the guard
-	move := true
-
-	for move == true {
-		move, currentPosition = doMove(currentPosition, guardMap)
-		visitedPositions = appendIfMissing(visitedPositions, pos2string(currentPosition))
-
-		if move {
-			path = append(path, currentPosition)
-		}
-	}
-
-	totalPositions = len(visitedPositions)
-	return totalPositions, path
-}
-
-func isLoop(testPath Path) bool {
-
-	pathMap := make(map[Position]int)
-	//fmt.Println(testPath)
-
-	for _, pos := range testPath {
-
-		// If the position doesnt exist in the map then we havent found a loop
-		//fmt.Println("Testiing pos: ", pos)
-		if _, ok := pathMap[pos]; !ok {
-			pathMap[pos] = 0
-
-		} else {
-			fmt.Printf("\nloop at %v\n", pos)
-			//fmt.Println(pathMap)
-			return true
-		}
-	}
-
-	return false
-}
-
-func copyPath(path []Position) []Position {
-	newSlice := make([]Position, len(path))
-	copy(newSlice, path)
-	return newSlice
 }
 
 func copyMap(original map[int][]rune) map[int][]rune {
@@ -343,90 +188,6 @@ func copyMap(original map[int][]rune) map[int][]rune {
 	return newMap
 }
 
-func part21(path Path, inputFile string) int {
-
-	loops := 0
-
-	guardMap := readInput(inputFile)
-
-	for i, currentPosition := range path {
-
-		newPos := currentPosition
-		move := true
-
-		loopTest := copyPath(path)
-
-		switch newPos.direction {
-		case "U":
-			fmt.Printf("Changing from %v to %s\n", newPos, "R")
-			newPos.direction = "R"
-
-		case "D":
-			fmt.Printf("Changing from %v to %s\n", newPos, "L")
-			newPos.direction = "L"
-
-		case "L":
-			fmt.Printf("Changing from %v to %s\n", newPos, "U")
-			newPos.direction = "U"
-
-		case "R":
-			fmt.Printf("Changing from %v to %s\n", newPos, "D")
-			newPos.direction = "D"
-
-		}
-
-		// Plot the new path then do the loop test
-
-		loopTest = loopTest[:i]
-
-		// There is an edge where the guard will oscillate between two positions. Need to detect and make
-		// an additional right turn to break the loop
-		infititeLoopCheck := map[Position]int{}
-
-		for move == true {
-
-			move, newPos = doMove(newPos, guardMap)
-
-			infititeLoopCheck[newPos] = infititeLoopCheck[newPos] + 1
-
-			if move {
-				loopTest = append(loopTest, newPos)
-			}
-
-			if infititeLoopCheck[newPos] > 2 {
-				//fmt.Println(newPos)
-
-				//fmt.Println(infititeLoopCheck[newPos])
-				switch newPos.direction {
-				case "U":
-					fmt.Printf("Inf loop fix %v to %s\n", newPos, "R")
-					newPos.direction = "R"
-
-				case "D":
-					fmt.Printf("Inf loop fix %v to %s\n", newPos, "L")
-					newPos.direction = "L"
-
-				case "L":
-					fmt.Printf("Inf loop fix %v to %s\n", newPos, "U")
-					newPos.direction = "U"
-
-				case "R":
-					fmt.Printf("Inf loop fix %v to %s\n", newPos, "D")
-					newPos.direction = "D"
-
-				}
-			}
-		}
-
-		if isLoop(loopTest) {
-			loops++
-		}
-
-	}
-
-	return loops
-}
-
 func printMap(guardMap map[int][]rune) {
 
 	for i := 0; i < len(guardMap); i++ {
@@ -436,154 +197,104 @@ func printMap(guardMap map[int][]rune) {
 	fmt.Println()
 }
 
-func part2(path Path, inputFile string) int {
+// Insert a new obstacle into the map at the given position
+func insertObstacle(guardMap map[int][]rune, pos Position) map[int][]rune {
+	newMap := copyMap(guardMap)
 
+	startPosition := findStart(guardMap)
+
+	if pos == startPosition {
+		return newMap
+	}
+
+	line := newMap[pos.y]
+	line[pos.x] = '#'
+	newMap[pos.y] = line
+
+	return newMap
+}
+
+// Test if a given map has a loop
+func testForLoop(guardMap map[int][]rune, startPos Position) bool {
+
+	newPos := startPos
+
+	visitedPositions := map[Position]struct{}{}
+
+	move := true
+
+	for move {
+
+		move, newPos = doMove(newPos, guardMap)
+
+		if !move {
+			return false
+		}
+
+		if _, ok := visitedPositions[newPos]; ok {
+
+			return true
+		}
+
+		visitedPositions[newPos] = struct{}{}
+
+	}
+
+	return false
+}
+
+func part1(inputfile string) int {
+	totalPositions := 0
+	guardMap := readInput(inputfile)
+
+	startPos := findStart(guardMap)
+
+	// Generate the path
+
+	path := getPath(guardMap, startPos)
+	totalPositions = countUniquePositions(path)
+
+	return totalPositions
+}
+
+func part2(inputfile string) int {
+	guardMap := readInput(inputfile)
 	loops := 0
-	startPosition := Position{0, 0, "U"}
 
-	guardMap := readInput(inputFile)
+	startPos := findStart(guardMap)
 
-	// Find the Staring position
-	for lineNo, line := range guardMap {
-		if strings.Contains(string(line), "^") {
-			guardPos := strings.Index(string(line), "^")
-			startPosition = Position{guardPos, lineNo, "U"}
-			break
-		}
-	}
+	// Get the unobstructed path
+	path := getPath(guardMap, startPos)
 
-	fmt.Println("Start Position: ", startPosition)
-	loopPos := make([]Position, 0)
+	posMap := make(map[string]struct{})
 
-	for _, currentPosition := range path {
-		tmpGuardMap := copyMap(guardMap)
+	for pos := range path {
 
-		switch currentPosition.direction {
+		posString := pos2string(pos)
 
-		case "U":
-			if currentPosition.y-1 >= 0 {
-				line := tmpGuardMap[currentPosition.y-1]
+		if _, ok := posMap[posString]; ok {
+			// Do nothing at the moment
+			// skip the position
 
-				if line[currentPosition.x] != '#' {
-					line[currentPosition.x] = '#'
-					tmpGuardMap[currentPosition.y-1] = line
-				} else {
-					// Move right
-					if currentPosition.x+1 < len(tmpGuardMap[currentPosition.y]) {
-						line := tmpGuardMap[currentPosition.y]
-						line[currentPosition.x+1] = '#'
-						tmpGuardMap[currentPosition.y] = line
-					}
-				}
+		} else {
+			testMap := insertObstacle(guardMap, pos)
+
+			if testForLoop(testMap, startPos) {
+				loops++
+
 			}
-
-		case "D":
-			if currentPosition.y+1 < len(tmpGuardMap) {
-				line := tmpGuardMap[currentPosition.y+1]
-
-				if line[currentPosition.x] != '#' {
-					line[currentPosition.x] = '#'
-					tmpGuardMap[currentPosition.y+1] = line
-				} else {
-					// Move left
-					if currentPosition.x-1 >= 0 {
-						line := tmpGuardMap[currentPosition.y]
-						line[currentPosition.x-1] = '#'
-						tmpGuardMap[currentPosition.y] = line
-					}
-				}
-			}
-
-		case "L":
-			if currentPosition.x-1 >= 0 {
-				line := tmpGuardMap[currentPosition.y]
-
-				if line[currentPosition.x-1] != '#' {
-					line[currentPosition.x-1] = '#'
-					tmpGuardMap[currentPosition.y] = line
-				} else {
-					// Move up
-					if currentPosition.y-1 >= 0 {
-						line := tmpGuardMap[currentPosition.y-1]
-						line[currentPosition.x] = '#'
-						tmpGuardMap[currentPosition.y-1] = line
-					}
-				}
-			}
-
-		case "R":
-			if currentPosition.x+1 < len(tmpGuardMap[currentPosition.y]) {
-				line := tmpGuardMap[currentPosition.y]
-
-				if line[currentPosition.x+1] != '#' {
-					line[currentPosition.x+1] = '#'
-					tmpGuardMap[currentPosition.y] = line
-				} else {
-					// Move down
-					if currentPosition.y+1 < len(tmpGuardMap) {
-						line := tmpGuardMap[currentPosition.y+1]
-						line[currentPosition.x] = '#'
-						tmpGuardMap[currentPosition.y+1] = line
-					}
-				}
-			}
-		}
-
-		printMap(tmpGuardMap)
-		newPos := startPosition
-		testPath := make(Path, 0)
-		//visitedPositions := make([]Position, 0)
-
-		move := true
-		for move == true {
-			move, newPos = doMove(newPos, tmpGuardMap)
-
-			if move {
-				testPath = append(testPath, newPos)
-			}
-
-		}
-
-		if isLoop(testPath) {
-			loops++
-
+			posMap[posString] = struct{}{}
 		}
 
 	}
 
-	fmt.Println(loopPos)
 	return loops
-}
-
-func isLoop2(testpath Path) bool {
-	visited := make(map[Position]bool)
-	for _, pos := range testpath {
-		if visited[pos] {
-			return true
-		}
-		visited[pos] = true
-	}
-	return false
-}
-
-func Contains(s []Position, e Position) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
 }
 
 func main() {
 
-	inputFile := "sample_input.txt"
-	p1Total, _ := part1(inputFile)
+	inputFile := "input.txt"
 
-	fmt.Printf("Part 1 Total: %d\n\n", p1Total)
-	//fmt.Printf("Part 2 Total: %v\n", part2(path, inputFile))
+	fmt.Printf("Part 1 Total: %d\n\n", part1(inputFile))
+	fmt.Printf("Part 2 Total: %v\n", part2(inputFile))
 }
-
-// 1908 too low
-// 1910 not right
-// 1933
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AOC2024
-Advent of Code 2024
+
+Advent of Code 2024. How far will I get?
